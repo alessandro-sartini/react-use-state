12cfa80f3f8786ee22498b994fc12cf2c121a93e
alessandro-sartini <ale.sartini.98@gmail.com>
2025-02-21 12:24:26 +0100

    Create: structures

---
+++ src/components/main/ButtonLanguages.jsx
@@ -0,0 +1,18 @@
+const ButtonLanguages = (props) => {
+    
+
+    const {id,title, description,isActive}= props
+
+    return (
+
+        <div className="btnContainer">
+
+            
+            <button>HTML</button>
+
+        </div>
+
+    )
+
+}
+export default ButtonLanguages
---
+++ src/components/main/Main.jsx
@@ -0,0 +1,22 @@
+import ButtonLanguages from "./ButtonLanguages"
+
+const Main = (props) => {
+    
+    return (
+        
+        <>
+            <h2>
+                Learn web development
+            </h2>
+            <ButtonLanguages langs={props.langs}/>
+            
+        
+        </>
+
+
+    )
+
+
+}
+
+export default Main 
